d118e6c13a21c91577ceed64823ea4d4905f8ed7
Samnieng1 <114794976+Samnieng1@users.noreply.github.com>
2024-07-12 21:36:28 +1200

    Added fetching data based on Genre Id

--- src/components/PopularGames.jsx
+++ src/components/PopularGames.jsx
@@ -1,21 +1,44 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
+import { FcRating } from "react-icons/fc";
+import { FaRegComment } from "react-icons/fa";
+import { ImFire } from "react-icons/im";
 
-function PopularGames({allGames}) {
-
-useEffect(()=>{
-console.log("allGame", allGames)
-},[])    
+function PopularGames({ allGames, selectedGenre }) {
+  useEffect(() => {
+    console.log("allGame", allGames);
+  }, []);
 
   return (
     <div>
-        {allGames.map((item)=>(
-            <div>
-                <img src={item.background_image}/>
-                <h2>{item.name}</h2>
+      <h2 className="text-xs lg:text-3xl font-bold">{selectedGenre} Popular Games</h2>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-4 gap-4'>
+        {allGames.map((item) => (
+          <div className='p-2  rounded-lg bg-red-100 group
+          hover:scale-110 transition-all duration-300 ease-in-out'>
+            <img
+              src={item.background_image}
+              className="h-[270px] w-full rounded-t-lg object-cover "
+            />
+            <h2 className="text-xs lg:text-base font-bold text-left  ">{item.name}</h2>
+            <div className="flex gap-4">
+              <div className="flex gap-2 items-center">
+                <FcRating />
+                <h2>{item.rating}</h2>
+              </div>
+              <div className="flex gap-2 items-center">
+                <FaRegComment />
+                <h2>{item.reviews_count}</h2>
+              </div>
+              <div className="flex gap-2 items-center">
+                <ImFire className="text-orange-400" />
+                <h2>{item.suggestions_count}</h2>
+              </div>
             </div>
+          </div>
         ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default PopularGames
+export default PopularGames;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -8,11 +8,14 @@ import PopularGames from "../components/PopularGames";
 function LandingPage() {
     //to store games list in a variable
     const [gameList,setGameList]=useState();
-    const[popularGameByGenre,setPopularGameByGenre]=useState();
+    const[popularGameByGenre,setPopularGameByGenre]=useState([]);
+    const[selectedGenre,setSelectedGenre]=useState('Action')//Action is the default genre
 //need to call the gelAllGames function here
 useEffect(()=>{
     getAllGames();
+    getPopularGamesByGenreID(4);
 },[])
+
 
 //to define a common function here to call game list so that we can pass it to any components. we don't need to call the game endpoint everytime.
 const getAllGames = ()=>{
@@ -23,8 +26,9 @@ setGameList(res.data.results) //now the game list has games data
 }
 //fetch based on genreID
 const getPopularGamesByGenreID=(id)=>{
-  Api.getGamesByGenreID(4).then((res)=>{
-    //console.log(res.data.results),
+  console.log("GENREID", id)
+  Api.getGamesByGenreID(id).then((res)=>{
+    console.log("Game List By Genre", res.data.results),
     setPopularGameByGenre(res.data.results)
   }
 
@@ -34,18 +38,20 @@ const getPopularGamesByGenreID=(id)=>{
   return (
     <div className="flex flex-col md:flex-row gap-2 ">
       <div className="bg-red-300 h-full w-full lg:w-1/4 ">
-      <Genre/>
+      <Genre genreId={(genreId)=>getPopularGamesByGenreID(genreId)} selectedGenre={(name)=>setSelectedGenre(name)}/>
       </div>
       <div className="w-full  lg:w-3/4">
       {/**pass the first element ([0]) of the gameList to Hero section. Prop name can be anything, here we name it as firstGame and the value to pass here is gameList
        * Then go to Hero.jsx to accept the prop firstGame.
        */}
        {/**add condiiton if gameList has data, show the Hero section. Otherwise, don't show anytihing */}
-      {gameList?.length>0?
+       {/* add condition as the getPopularGamesByGenreID may take sometimes to load && and popularGameByGenre to wait to load so that when we have all the pupular game by genre then we will see the data */}
+      {gameList?.length>0&&popularGameByGenre.length>0?
       <div className="flex flex-col gap-4">
       <Hero firstGame={gameList[0]}/>
       <TrendingGames allGames={gameList}/>
-      {/* <PopularGames allGames={popularGameByGenre} /> */}
+       <PopularGames allGames={popularGameByGenre}
+       selectedGenre={selectedGenre} />
       </div>
       :null}
       
